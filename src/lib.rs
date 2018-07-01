ede85a96b00e98cb52184facb1b9685060a1057e
Tomas Ostasevicius <t.ostasevicius@gmail.com>
2018-07-01 21:06:03 +0200

    Actually minimizes as expected

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ extern crate num_traits;
 
 use float_cmp::{ApproxEqUlps, ApproxOrdUlps};
 use ndarray::prelude::*;
+use ndarray::Zip;
 
 static ULPS: i64 = 1;
 
@@ -41,7 +42,7 @@ impl Minimizer {
         F: Fn(ArrayView1<f64>) -> f64,
     {
         let mut x0 = args.to_owned();
-        let N: usize = x0.len();
+        let n: usize = x0.len();
 
         let rho = 1f64;
         let chi = 2f64;
@@ -49,11 +50,11 @@ impl Minimizer {
         let sigma = 0.5f64;
         let xatol = 1e-4f64;
 
-        let mut sim = Array2::<f64>::zeros((N + 1, N));
+        let mut sim = Array2::<f64>::zeros((n + 1, n));
         let nonzdelt = 0.05f64;
         let zdelt = 0.00025f64;
         sim.slice_mut(s![0, ..]).assign(&x0);
-        for k in 0..N {
+        for k in 0..n {
             let mut y = x0.clone();
             y[k] = match y[k].approx_eq_ulps(&0., ULPS) {
                 true => zdelt,
@@ -61,19 +62,20 @@ impl Minimizer {
             };
             sim.slice_mut(s![k + 1, ..]).assign(&y);
         }
-        let maxiter = 200 * N;
-        let mut fsim = Array1::<f64>::zeros(N + 1);
+        let maxiter = 200 * n;
+        let mut fsim = Array1::<f64>::zeros(n + 1);
 
-        // TODO: Optimize with iterators / zips
-        for k in 0..N + 1 {
-            fsim[k] = func(sim.slice(s![k, ..]));
-        }
+        Zip::from(&mut fsim).and(sim.genrows()).apply(|f, s| {
+            *f = func(s);
+        });
+
         order_simplex(sim.view_mut(), fsim.view_mut());
-
         let mut iterations: usize = 1;
+        let s0 = s![0, ..];
+        let sm1 = s![-1, ..];
 
         while iterations < maxiter {
-            if (&sim.slice(s![1.., ..]) - &sim.slice(s![0, ..]))
+            if (&sim.slice(s![1.., ..]) - &sim.slice(s0))
                 .mapv(f64::abs)
                 .fold(0., |acc, &x| match acc.approx_gt_ulps(&x, ULPS) {
                     true => acc,
@@ -85,66 +87,61 @@ impl Minimizer {
             }
 
             let xbar = sim.slice(s![..-1, ..])
-                .gencolumns()
+                .genrows()
                 .into_iter()
-                .fold(Array1::<f64>::zeros(N), |acc, x| acc + x);
+                .fold(Array1::<f64>::zeros(n), |acc, x| acc + x) / n as f64;
 
-            // println!("Iteration number {}:", iterations);
-            // println!("sim: {}", sim);
-            // println!("fsim: {}", fsim);
-            // println!("======================");
-
-            let xr = &xbar * (rho + 1.) - &sim.slice(s![-1, ..]) * rho;
+            let xr = &xbar * (rho + 1.) - (&sim.slice(sm1) * rho);
             let fxr = func(xr.view());
+
             let mut doshrink = false;
 
             if fxr.approx_lt_ulps(&fsim[0], ULPS) {
-                let xe = 1. + rho * chi * &xbar - rho * chi * &sim.slice(s![-1, ..]);
+                let xe = (1. + rho * chi) * &xbar - (rho * chi * &sim.slice(sm1));
                 let fxe = func(xe.view());
 
                 if fxe.approx_lt_ulps(&fxr, ULPS) {
-                    sim.slice_mut(s![-1, ..]).assign(&xe);
-                    fsim[N] = fxe;
+                    sim.slice_mut(sm1).assign(&xe);
+                    fsim[n] = fxe;
                 } else {
-                    sim.slice_mut(s![-1, ..]).assign(&xr);
-                    fsim[N] = fxr;
+                    sim.slice_mut(sm1).assign(&xr);
+                    fsim[n] = fxr;
                 }
             } else {
-                if fxr.approx_lt_ulps(&fsim[N - 1], ULPS) {
-                    sim.slice_mut(s![-1, ..]).assign(&xr);
-                    fsim[N] = fxr;
+                if fxr.approx_lt_ulps(&fsim[n - 1], ULPS) {
+                    sim.slice_mut(sm1).assign(&xr);
+                    fsim[n] = fxr;
                 } else {
-                    if fxr.approx_lt_ulps(&fsim[N], ULPS) {
-
-                        let xc = (1. + psi * rho) * &xbar - psi * rho * &sim.slice(s![-1, ..]);
+                    if fxr.approx_lt_ulps(&fsim[n], ULPS) {
+                        let xc = (1. + psi * rho) * &xbar - psi * rho * &sim.slice(sm1);
                         let fxc = func(xc.view());
 
                         if fxc.approx_gt_ulps(&fxr, ULPS) {
                             doshrink = true;
                         } else {
-                            sim.slice_mut(s![-1, ..]).assign(&xc);
-                            fsim[N] = fxc;
+                            sim.slice_mut(sm1).assign(&xc);
+                            fsim[n] = fxc;
                         }
                     } else {
-
-                        let xcc = (1. - psi) * &xbar + psi * &sim.slice(s![-1, ..]);
+                        let xcc = (1. - psi) * &xbar + psi * &sim.slice(sm1);
                         let fxcc = func(xcc.view());
 
-                        if fxcc.approx_lt_ulps(&fsim[N], ULPS) {
-                            sim.slice_mut(s![-1, ..]).assign(&xcc);
-                            fsim[N] = fxcc;
+                        if fxcc.approx_lt_ulps(&fsim[n], ULPS) {
+                            sim.slice_mut(sm1).assign(&xcc);
+                            fsim[n] = fxcc;
                         } else {
                             doshrink = true;
                         }
                     }
 
                     if doshrink {
-                        for j in 1..N + 1 {
-                            let mut _tmp = &sim.slice(s![j, ..]) - &sim.slice(s![0, ..]);
+                        for j in 1..n + 1 {
+                            let sj = s![j, ..];
+                            let mut _tmp = &sim.slice(sj) - &sim.slice(s0);
                             _tmp *= sigma;
-                            _tmp += &sim.slice(s![0, ..]);
-                            sim.slice_mut(s![j, ..]).assign(&_tmp);
-                            fsim[j] = func(sim.slice(s![j, ..]).view());
+                            _tmp += &sim.slice(s0);
+                            sim.slice_mut(sj).assign(&_tmp);
+                            fsim[j] = func(sim.slice(sj).view());
                         }
                     }
                 }
@@ -154,6 +151,7 @@ impl Minimizer {
             iterations += 1;
         }
         x0.assign(&sim.slice(s![0, ..]));
+        println!("Final value: {}\nArgs:\n{}", fsim[0], x0);
         x0
     }
 
