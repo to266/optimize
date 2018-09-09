0a887b279df39b7f3fe4f988642f992a2353bc21
Tomas Ostasevicius <t.ostasevicius@gmail.com>
2018-09-09 19:24:52 +0200

    Added approx_prime and unit test

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-//! This crate provides (non-linear) numerical optimization methods. 
+//! This crate provides (non-linear) numerical optimization methods.
 //!
 //! It is heavily based on `scipy.optimize`.
 //!
@@ -212,7 +212,8 @@ impl Minimizer for NelderMead {
                 break;
             }
 
-            let xbar = sim.slice(s![..-1, ..])
+            let xbar = sim
+                .slice(s![..-1, ..])
                 .genrows()
                 .into_iter()
                 .fold(Array1::<f64>::zeros(n), |acc, x| acc + x) / n as f64;
@@ -281,6 +282,24 @@ impl Minimizer for NelderMead {
     }
 }
 
+fn approx_fprime<F>(xk: ArrayView1<f64>, func: F, epsilon: ArrayView1<f64>) -> Array1<f64>
+where
+    F: Fn(ArrayView1<f64>) -> f64,
+{
+    let f0 = func(xk);
+    let n = xk.len();
+    let mut grad = Array1::<f64>::zeros(n);
+    let mut ei = Array1::<f64>::zeros(n);
+    let mut d = Array1::<f64>::zeros(n);
+    for k in 0..n {
+        ei[k] = 1.0;
+        d.assign(&(&ei * &epsilon));
+        grad[k] = (func((&d + &xk).view()) - &f0) / &d[k];
+        ei[k] = 0.0;
+    }
+    grad
+}
+
 #[cfg(test)]
 mod tests {
 
@@ -301,6 +320,18 @@ mod tests {
         println!("res: {}", res);
         assert!(res[0].approx_eq(&1.0, 1e-4, 10));
         assert!(res[1].approx_eq(&1.0, 1e-4, 10));
+    }
+
+    #[test]
+    fn gradient() {
+        let function = |x: ArrayView1<f64>| 1.0 * x[0].powi(2) + 200. * x[1].powi(2);
+        let x = Array::from_vec(vec![1.0, 1.0]);
+        let eps_ar = Array::from_vec(vec![1e-7, 14.14 * 1e-7]);
+        let res = approx_fprime(x.view(), function, eps_ar.view());
+
+        println!("Res: {}", res);
+        assert!(res[0].approx_eq(&2.0, 1e-4, 10));
+        assert!(res[1].approx_eq(&400.0, 1e-3, 10));
     }
 
 }
