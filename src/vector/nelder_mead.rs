b53a31dc5c495c907ca71f0691c40e94fe111df9
sebasv <mail@sebastiaanvermeulen.nl>
2018-10-10 20:50:31 +0200

    docstrings and code cleanup

--- src/vector/nelder_mead.rs
+++ src/vector/nelder_mead.rs
@@ -1,5 +1,40 @@
+//! This implementation of Nelder-Mead is based on
+//!  
+//! Gao, F and Han, L. Implementing the Nelder-Mead simplex algorithm with 
+//! adaptive parameters. 2012. Computational Optimization and Applications. 
+//! 51:1, pp 259--277
+//! 
+//! In particular, it adapts their suggestion to use adaptive step sizes,
+//! which depend on the dimensionality of the optimization problem.
+//! 
+//! # Use case
+//! 
+//! The Nelder-Mead algorithm does not require a gradient or a hessian.
+//! As a tradeoff it typically requires a lot of function evaluations to 
+//! find a minimum. Further, there are few theoretical results on the 
+//! convergence of Nelder-Mead iterations.
+//! 
+//! # Examples
+//! 
+//! ```
+//! # extern crate ndarray;
+//! # extern crate optimize;
+//! # use ndarray::prelude::*;
+//! # use optimize::vector::NelderMeadBuilder;
+//! 
+//! let function =
+//!     |x: ArrayView1<f64>| (1.0 - x[0]).powi(2) + 100.0 * (x[1] - x[0].powi(2)).powi(2);
+//! let minimizer = NelderMeadBuilder::default()
+//!     .xtol(1e-8)
+//!     .ftol(1e-8)
+//!     .build()
+//!     .unwrap();
+//! let args = Array::from_vec(vec![3.0, -8.3]);
+//! let res = minimizer.minimize_x0(&function, args.view());
+//! println!("res: {}", res);
+//! ```
 
-use float_cmp::{ApproxOrdUlps, ApproxEqUlps};
+use float_cmp::ApproxOrdUlps;
 use ndarray::prelude::*;
 
 type Simplex = Vec<(f64, Array1<f64>)>;
@@ -51,6 +86,9 @@ pub struct NelderMead {
 
 impl NelderMead {
 
+    /// Search for the value minimizing `func` given an initial guess
+    /// in the form of a point. The algorithm will explore the variable
+    /// space without constraints.
     pub fn minimize_x0<F>(&self, func: F, x0: ArrayView1<f64>) -> Array1<f64>
     where F: Fn(ArrayView1<f64>) -> f64 {
         let n = x0.len();
@@ -62,57 +100,65 @@ impl NelderMead {
         self.minimize_simplex(func, init_simplex)
     }
 
+    /// Search for the value minimizing `func` given an initial guess
+    /// in the form of a set of coordinates, the `init_simplex`. This algorithm
+    /// only ever explores the space spanned by these initial vectors.
+    /// If you have parameter restrictions that effectively place your parameters 
+    /// in a subspace, you can enforce these restrictions by setting `init_simplex`
+    /// to a basis of this subspace.
     pub fn minimize_simplex<F>(&self, func: F, init_simplex: Array2<f64>) -> Array1<f64>
     where F: Fn(ArrayView1<f64>) -> f64 {
-        let n = init_simplex.shape()[1];
         let mut func = WrappedFunction { num: 0, func: func };
         let mut simplex = init_simplex.outer_iter().map(|xi| (func.call(xi), xi.to_owned())).collect::<Simplex>();
         self.order_simplex(&mut simplex);
         let mut centroid = self.centroid(&simplex);
+        let n = simplex.len();
 
-        let (maxiter, maxfun, rho, chi, psi, sigma) = self.initialize_parameters(n);
+        let (maxiter, maxfun, alpha, beta, gamma, delta) = self.initialize_parameters(n);
 
         let mut iterations = 1;
 
         while !self.finished(&simplex, iterations, maxiter, func.num, maxfun) {
 
-            let f_worst = simplex[n-1].0;
-            let f_best = simplex[0].0;
-            eprintln!("{}: {}", iterations, f_best);
+            let f_n1 = simplex[n-1].0;
+            let f_n = simplex[n-2].0;
+            let f_0 = simplex[0].0;
 
-            let reflected = &centroid + &(rho * &(&centroid - &simplex[n-1].1));
+            let reflected = &centroid + &(alpha * &(&centroid - &simplex[n-1].1));
             let f_reflected = func.call(reflected.view());
 
-            if f_reflected < f_worst && f_reflected > f_best { // try reflecting the worst point through the centroid
-                eprintln!("reflect");
+            if f_reflected < f_n && f_reflected > f_0 { // try reflecting the worst point through the centroid
                 self.lean_update(&mut simplex, &mut centroid, reflected, f_reflected);
-            } else if f_reflected < f_best { // try expanding beyond the centroid
-                let expanded = &centroid + &(chi * &(&centroid - &reflected));
+            } else if f_reflected < f_0 { // try expanding beyond the centroid
+                let expanded = &centroid + &(beta * &(&reflected - &centroid));
                 let f_expanded = func.call(expanded.view());
 
                 if f_expanded < f_reflected {
-                eprintln!("expand");
                     self.lean_update(&mut simplex, &mut centroid, expanded, f_expanded);
                 } else {
                     self.lean_update(&mut simplex, &mut centroid, reflected, f_reflected);
-                eprintln!("reflect");
                 }
-            } else { // try a contraction towards the centroid
-                let contracted = &centroid + &(psi * (&centroid - &simplex[n-1].1));
+            } else if f_reflected < f_n1 && f_reflected >= f_n { // try a contraction outwards
+                let contracted = &centroid + &(gamma * (&centroid - &simplex[n-1].1));
+                let f_contracted = func.call(contracted.view());
+                if f_contracted < f_reflected {
+                    self.lean_update(&mut simplex, &mut centroid, contracted, f_contracted);
+                } else {
+                    // shrink
+                    self.shrink(&mut simplex, &mut func, delta, &mut centroid);
+                }
+            } else { // try a contraction inwards
+                let contracted = &centroid - &(gamma * (&centroid - &simplex[n-1].1));
                 let f_contracted = func.call(contracted.view());
 
-                if f_contracted < f_worst {
+                if f_contracted < f_reflected {
                     self.lean_update(&mut simplex, &mut centroid, contracted, f_contracted);
-                    eprintln!("contract");
-                } else { // shrink if all else fails
-                    eprintln!("shrink");
-                    self.shrink(&mut simplex, &mut func, sigma);
-                    self.order_simplex(&mut simplex);
-                    centroid = self.centroid(&mut simplex);
+                } else {
+                    // shrink
+                    self.shrink(&mut simplex, &mut func, delta, &mut centroid);
                 }
-            }        
+            }
             iterations += 1;
-            eprintln!("{:?}", simplex.iter().map( |(f,_)| *f).collect::<Vec<f64>>());
         }
         simplex.remove(0).1
     }
@@ -130,20 +176,19 @@ impl NelderMead {
             None => 200 * n
         };
 
-        let (rho, chi, psi, sigma) = if self.adaptive {
+        let (alpha, beta, gamma, delta) = if self.adaptive {
             let dim = n as f64;
             (1.0, 1.0 + 2.0 / dim, 0.75 - 1.0 / (2.0 * dim), 1.0 - 1.0 / dim)
         } else {
             (1.0, 2.0, 0.5, 0.5)
         };
 
-        (maxiter, maxfun, rho, chi, psi, sigma)
+        (maxiter, maxfun, alpha, beta, gamma, delta)
     }
 
     #[inline]
     fn finished(&self, simplex: &Simplex, iterations: usize, maxiter: usize, nfeval: usize, maxfun: usize) -> bool {
         let n = simplex.len();
-        eprintln!("{},{},{},{}", iterations, nfeval, simplex[n-1].0 - simplex[0].0, (&simplex[n-1].1 - &simplex[0].1).mapv(f64::abs).scalar_sum());
         if iterations > maxiter || nfeval > maxfun {
             true
         } else if simplex[n-1].0 - simplex[0].0 < self.ftol 
@@ -157,6 +202,7 @@ impl NelderMead {
     /// Update the centroid effiently, knowing only one value changed.
     /// The pattern-defeating sort of order_simplex is allready efficient
     /// given that we inserted a single out-of-place value in a sorted vec.
+    /// This update is O(n).
     #[inline]
     fn lean_update(&self, simplex: &mut Simplex, centroid: &mut Array1<f64>, xnew: Array1<f64>, fnew: f64) {
         let n = simplex.len();
@@ -167,21 +213,33 @@ impl NelderMead {
     }
 
     /// shrink all points towards the best point.
-    /// Assumes the simplex is ordered.
+    /// Assumes the simplex is ordered. 
+    /// The centroid is updated by shrinking the centroid directly,
+    /// Then removing the new 'worst x' and adding in the old 'worst x'.
+    /// This update of `centroid` is O(n). 
+    /// Shrinkage requires n function evaluations.
     #[inline]
-    fn shrink<F>(&self, simplex: &mut Simplex, f: &mut WrappedFunction<F>, sigma: f64) 
+    fn shrink<F>(&self, simplex: &mut Simplex, f: &mut WrappedFunction<F>, sigma: f64, centroid: &mut Array1<f64>)
     where F: Fn(ArrayView1<f64>) -> f64 {
-        let mut iter = simplex.iter_mut();
-        let (_, x0) = iter.next().unwrap();
-        for (fi, xi) in iter {
-            *xi *= sigma;
-            *xi += &((1.0 - sigma) * &x0.view());
-            *fi = f.call(xi.view());
+        {
+            let mut iter = simplex.iter_mut();
+            let (_, x0) = iter.next().unwrap();
+            for (fi, xi) in iter {
+                *xi *= sigma;
+                *xi += &((1.0 - sigma) * &x0.view());
+                *fi = f.call(xi.view());
+            }
         }
+        let n = simplex.len() - 1;
+        let old_worst = simplex[n - 1].1.to_owned();
+        *centroid *= sigma;
+        *centroid += &((1.0 - sigma) * &simplex[0].1);
+        self.order_simplex(simplex);
+        *centroid += &((&old_worst - &simplex[n - 1].1) / (n - 1) as f64);
     }
 
     /// calculate the centroid of all points but the worst one.
-    /// Assumes that the simplex is ordered.
+    /// Assumes that the simplex is ordered. This calculation is O(n^2).
     #[inline]
     fn centroid(&self, simplex: &Simplex) -> Array1<f64> {
         let n = simplex.len();
